9f6c87ee6f10d2aaef76c0893527e4706b63ad80
mathfiliph22 <matheusfiliph@hotmail.com>
2021-12-01 21:11:50 -0300

    j-query

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
+$(document).ready(function() {
+    $("#carrosel img:eq(0)").addClass("banner ativo").show()
+
+    setInterval(slide, 3000)
+
+    function slide () {
+        if( $(".banner-ativo").next().length ) {
+            $(".banner-ativo").removeClass("banner-ativo").fadeOut().next().addClass("banner-ativo").fadeIn()
+            } else {
+                $(".banner-ativo").removeClass().fadeOut()
+                $("#carrosel img:eq(0)").addClass("banner-ativo").fadeIn()
+            }
+              
+    }
+
+})
+
+
+
+
+
+
+
 let email = document.getElementById("campo-email");
 
 function enviarEmail(){
